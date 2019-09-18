aaa32e619291a716ad8e4b538f4063b1ca4d112b
Soumyadeep21 <soumo.sinha21@gmail.com>
2019-09-19 01:30:37 +0530

    sliver added

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -3,11 +3,20 @@ import 'package:provider/provider.dart';
 import 'package:wallpaper_app/config/home_page_config.dart';
 import 'package:wallpaper_app/universal/base_page.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
 
   @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> with AutomaticKeepAliveClientMixin{
+  @override
   Widget build(BuildContext context) {
+    super.build(context);
     HomePageConfig config = Provider.of<HomePageConfig>(context);
-    return BasePage(config: config);
+    return BasePage(config: config,appBarTitle: Text('Pixels : Wallpapers for you'));
   }
+
+  @override
+  bool get wantKeepAlive => true;
 }
