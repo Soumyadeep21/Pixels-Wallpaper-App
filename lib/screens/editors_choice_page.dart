984c67f4202d81639adcd300ede9fc596608997a
Soumyadeep21 <soumo.sinha21@gmail.com>
2019-09-25 23:46:03 +0530

    some changes done

--- lib/screens/editors_choice_page.dart
+++ lib/screens/editors_choice_page.dart
@@ -3,25 +3,23 @@ import 'package:provider/provider.dart';
 import 'package:wallpaper_app/config/editors_choice_page_config.dart';
 import 'package:wallpaper_app/universal/base_page.dart';
 
-class EditorsChoicePage extends StatefulWidget{
+class EditorsChoicePage extends StatefulWidget {
   @override
   _EditorsChoicePageState createState() => _EditorsChoicePageState();
 }
 
-class _EditorsChoicePageState extends State<EditorsChoicePage> with AutomaticKeepAliveClientMixin{
+class _EditorsChoicePageState extends State<EditorsChoicePage>
+    with AutomaticKeepAliveClientMixin {
   @override
   Widget build(BuildContext context) {
     super.build(context);
     EditorsChoicePageConfig config =
         Provider.of<EditorsChoicePageConfig>(context);
-
+    config.getData();
     return BasePage(
       config: config,
       appBarTitle: Row(
-        children: <Widget>[
-          Icon(Icons.star),
-          Text('Editor\'s Choice')
-        ],
+        children: <Widget>[Icon(Icons.star), Text('Editor\'s Choice')],
       ),
     );
   }
