ef4e0bb5edab9b8668ad87f7470350f4738f05c7
Soumyadeep21 <soumo.sinha21@gmail.com>
2019-09-25 23:46:46 +0530

    search function implemented

--- lib/config/search_page_config.dart
+++ lib/config/search_page_config.dart
@@ -1,7 +1,7 @@
 import 'package:wallpaper_app/config/base_page_config.dart';
 
 class SearchPageConfig extends BasePageConfig {
-  Future<void> getData({bool editorsChoice,String query}) async => await super.getData(query: query);
+  @override Future<void> getData({bool editorsChoice,String query}) async => await super.getData(query: query);
 
   Future<void> getMoreData({bool editorsChoice,String query}) async => await super.getMoreData(query: query);
 
