a9920375d15363aef9524dda1f2a1a382693822d
Soumyadeep21 <soumo.sinha21@gmail.com>
2019-09-30 00:13:10 +0530

    version updated

--- lib/utils/wallpaper_app.dart
+++ lib/utils/wallpaper_app.dart
@@ -2,7 +2,7 @@ import 'package:shared_preferences/shared_preferences.dart';
 
 class WallpaperApp{
   static const String appName = 'Pixels';
-  static const String appVersion = '1.0.1';
+  static const String appVersion = '1.0.2';
   static const bool isDebugMode = false;
 
   static const String notoSans = 'NotoSans';
